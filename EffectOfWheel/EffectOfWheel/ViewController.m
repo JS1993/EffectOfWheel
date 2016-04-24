f7fd740e0b503ba8cf1cb728377c638d94a25389
susu <28025920@qq.com>
2016-04-24 21:06:11 +0800

    创建项目，添加素材

---
+++ EffectOfWheel/EffectOfWheel/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  EffectOfWheel
+//
+//  Created by  江苏 on 16/4/24.
+//  Copyright © 2016年 jiangsu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+}
+
+
+
+@end
